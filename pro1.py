cc3fc3704a8275bbf9661ff29a1f297e62be262f
Sugal-3003 <52417450+Sugal-3003@users.noreply.github.com>
2019-07-20 23:36:21 +0530

    Update pro1.py

--- pro1.py
+++ pro1.py
@@ -1,10 +1,11 @@
-a=int(input())
-b=[]
-for i in range(0,a):
+
+b=int(input())
+c=[]
+for i in range(0,b):
  s=input()
- b.append(s)
+ c.append(s)
 l=[]
-for i in zip(*b):
+for i in zip(*c):
     if(i.count(i[0])==len(i)):
         l.append(i[0])
     else:
