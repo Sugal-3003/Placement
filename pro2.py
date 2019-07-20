ec7c346354cb359f77792363fb62e7e8772f8ba9
Sugal-3003 <52417450+Sugal-3003@users.noreply.github.com>
2019-07-21 00:50:27 +0530

    Update pro2.py

--- pro2.py
+++ pro2.py
@@ -1,8 +1,8 @@
 from itertools import combinations
-n,n2=input().split()
+n3,n2=input().split()
 n2=int(n2)
 l=[]
-dd=combinations(n,len(n)-n2)
+dd=combinations(n3,len(n3)-n2)
 for i in list(dd):
     l.append("".join(i))
 print(min(l))
